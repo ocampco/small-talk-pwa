2be6caea691e8a0d20772e4504def3ece72632eb
ocampco <ocampco@gmail.com>
2019-04-20 21:14:49 +1000

    Add production assets

---
+++ build/precache-manifest.d68baca0d5fbdb69ffe4aa5b4fbd94e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc6c1dcd7d7c7a57d36e0b0cd3b1815e",
+    "url": "/small-talk-pwa/static/media/translate.fc6c1dcd.svg"
+  },
+  {
+    "revision": "443d813aa962845ad2ccf00747842b38",
+    "url": "/small-talk-pwa/static/media/swap.443d813a.svg"
+  },
+  {
+    "revision": "bb806ce630e10984ae87",
+    "url": "/small-talk-pwa/static/js/runtime~main.bb806ce6.js"
+  },
+  {
+    "revision": "16160a7c6921087e4901",
+    "url": "/small-talk-pwa/static/js/main.16160a7c.chunk.js"
+  },
+  {
+    "revision": "60b1587c0ce3ea21e648",
+    "url": "/small-talk-pwa/static/js/2.60b1587c.chunk.js"
+  },
+  {
+    "revision": "16160a7c6921087e4901",
+    "url": "/small-talk-pwa/static/css/main.a621bbcb.chunk.css"
+  },
+  {
+    "revision": "f7b9c28245487fa8cfc0032d9f9310c3",
+    "url": "/small-talk-pwa/index.html"
+  }
+];
